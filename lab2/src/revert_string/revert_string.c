a1de0ba5b743e68b435a3d334b13f0b255cb0f94
benaxxx <benaxxx@mail.ru>
2021-03-10 20:53:57 +0000

    lab2 commit

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,13 @@
+#include <string.h>
 #include "revert_string.h"
 
 void RevertString(char *str)
 {
-	// your code here
+    for (unsigned long i=0; i < (strlen(str)/2); i++)
+    {
+        *(str+i) = *(str+i) - *(str+strlen(str)-1-i); 
+        *(str+strlen(str)-1-i) = *(str+i) + *(str+strlen(str)-1-i); 
+        *(str+i) = *(str+strlen(str)-1-i) - *(str+i);
+    }
 }
 
